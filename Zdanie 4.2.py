2b211f072fa13bfa25b1b835ea90609925e241e3
Robert Starosta <Robert.starosta@o2.pl>
2021-03-28 19:39:14 +0200

    Add main and change def name

--- Zdanie 4.2.py
+++ Zdanie 4.2.py
@@ -1,8 +1,13 @@
-def check_palindrom(palindrom):
+def is_palindrom(palindrom):
     """
     :param palindrom: Podaj słowo do sprawdzenia
     :return: zwraca prawda lub fałsz
     """
-    print (palindrom == palindrom[::-1])
-help(check_palindrom)
-check_palindrom("kajak")
+    if (palindrom == palindrom[::-1]):
+        print("Słowo jest palindromem")
+    else:
+        print("Słowo nie jest palindromem")
+
+if __name__ == "__main__":
+    palidrom = input("Podaj słowo do sprawdzenia: ")
+    is_palindrom(palidrom)
