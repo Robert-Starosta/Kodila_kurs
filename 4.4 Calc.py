92fed2130cdf4ef9664a417ae2483bc3734ea123
Robert Starosta <Robert.starosta@o2.pl>
2021-03-29 19:43:56 +0200

    Add logging and debug

--- 4.4 Calc.py
+++ 4.4 Calc.py
@@ -1,21 +1,29 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 def sum (a, b):
+    logging.debug("Ten program dodał do siebie liczbę %i oraz %i" % (a, b))
     print (a + b)
 def difference(a, b):
+    logging.debug("Ten program odjoł do siebie liczbę %i oraz %i" % (a, b))
     print (a - b)
 def multiplication(a, b):
     print (a * b)
+    logging.debug("Ten program pomnożył dwie liczby %i oraz %i" % (a, b))
 def division(a, b):
-    print (a / b)
+    logging.debug("Ten program podzielił przez siebie liczby %i oraz %i" % (a, b))
+    if b == 0:
+        print("Nie można wykonanć działania dzielenie przez 0 niemożliwe")
+    else:
+        print (a / b)
 
 
 
 if __name__ == "__main__":
     calc_function = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
     calc_function = int(calc_function)
-    a = input("Podaj pierwsza liczbę: ")
-    b = input("Podaj drugą liczbę: ")
-    a = int(a)
-    b = int(b)
+    a = int(input("Podaj pierwsza liczbę: "))
+    b = int(input("Podaj drugą liczbę: "))
     if calc_function == 1:
         sum(a, b)
     elif calc_function == 2:
@@ -23,10 +31,7 @@ if __name__ == "__main__":
     elif calc_function == 3:
         multiplication(a, b)
     elif calc_function == 4:
-        if b == 0:
-            print("Nie można wykonanć działania dzielenie przez 0 niemożliwe")
-        else:
-            division(a, b)
+        division(a, b)
 
 
 
